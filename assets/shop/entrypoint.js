48c248601116f748e1a5c34d19846c1038a9713f
lisiaal <aldoal18@gmail.com>
2024-12-23 16:20:09 +0100

    change step

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,2 +1,16 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
+document.addEventListener('DOMContentLoaded', function () {
+const quantity = document.getElementById('sylius_shop_add_to_cart_cartItem_quantity');
+if (quantity) {
+    quantity.value = 10;
+    quantity.setAttribute('step', '10');
+    quantity.setAttribute('min', '10');
+
+    quantity.addEventListener('change', function() {
+        if (parseInt(quantity.value) === 70) {
+            alert('Great Choice!');
+        }
+    });
+}
+});
